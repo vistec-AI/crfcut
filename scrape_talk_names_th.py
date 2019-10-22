5d80e8d084d3786c69cfd53a1a8b17646b8ca230
Saiko Yoneyabashi <artificialcc@gmail.com>
2019-10-22 11:32:21 +0700

    Change output file name

--- scrape_talk_names_th.py
+++ scrape_talk_names_th.py
@@ -61,5 +61,5 @@ if __name__ == '__main__':
 
     current_time = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M")
 
-    with open('./data/talk.names.with-th-transcript.{}.json'.format(current_time), 'w', encoding="utf-8") as f:
+    with open('./data/talks.names.with-th-transcript.{}.json'.format(current_time), 'w', encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
